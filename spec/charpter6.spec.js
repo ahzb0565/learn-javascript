76cd67afc6e7bb790c170890d7b839d5ecb88ab6
Bob <ahzb0565@sina.com>
2017-11-02 01:53:48 +0800

    6.4 check properties

--- spec/charpter6.spec.js
+++ spec/charpter6.spec.js
@@ -2,15 +2,16 @@
 
 describe('Charpter 6: Inherit', function(){
     var inherit = require('../lib/myLib');
+    var book;
+
+    beforeEach(function(){
+        book = {title: 'my book', author: 'Bob'};
+    });
 
     describe('6.1 create object', function(){});
 
     describe('6.2 get and set properties of object', function(){
-        var book;
-
-        beforeEach(function(){
-            book = {title: 'my book', author: 'Bob'};
-        });
+        
 
         it('undefined property', function(){
             expect(book.test).toBe(undefined);
@@ -29,7 +30,7 @@ describe('Charpter 6: Inherit', function(){
             expect(book.title).toBe('my book');
         });
 
-        it('to ways to get or set properties', function(){
+        it('two ways to get or set properties', function(){
             expect(book.title).toBe('my book');
             expect(book['title']).toEqual(book.title);
 
@@ -62,15 +63,62 @@ describe('Charpter 6: Inherit', function(){
 
         it('Override properties', function(){
             var myBook = inherit(book);
-            myBook.year = 2017;
-            expect(myBook.year).toBe(2017);
-            expect(book.year).toBeUndefined();
+            myBook.title = 2017;
+            expect(myBook.title).toBe(2017);
+            expect(book.title).toBe('my book');
         });
 
         it('not throw error when get wrong property name', function(){
             expect(function(){book.notExists}).not.toThrow();
-            expect(function(){book.notExists.notExists}).toThrow();
+            expect(function(){book.notExists.notExists}).toThrow();  // Can't get or set properties of undefined.
         })
+    });
+
+    describe('6.3 delete properties', function(){
+        it('Can only delete native properties', function(){
+            var myBook = inherit(book);
+            myBook.test = 'test';
+            expect(delete myBook.test).toBe(true);  // return ture and delete native property 'test'
+            expect(delete myBook.title).toBe(true);  // return true but do nothing
+            expect(myBook.test).toBeUndefined();
+            expect(myBook.title).toBe('my book');
+        });
+
+        it('special delete result', function(){
+            expect(delete book.title).toBe(true); // return true and delte 'title'
+            expect(delete book.title).toBe(true); // 'title' deleted already, so do nothing and return true
+            expect(delete book.toString).toBe(true); // ''toString' is a inherited function, do nothing and return true
+            expect(delete 1).toBe(true); // make no sense
+        });
+    });
+
+    describe('6.4 check properties', function(){
+        it('check if object have this property: in', function(){
+            var myBook = inherit(book);
+            myBook.test = 'test';
+            expect('title' in myBook).toBe(true);
+            expect('test' in myBook).toBe(true);
+            expect('notExists' in myBook).toBe(false);
+        });
+
+        it('check if object have this property: !==', function(){
+            var myBook = inherit(book);
+            myBook.test = 'test';
+            myBook.x = undefined;  // assign undefined
+            myBook.y = null;  // assign null
+            expect(myBook.title !== undefined).toBe(true);
+            expect(myBook.test !== undefined).toBe(true);
+            expect(myBook.x !== undefined).toBe(false);
+            expect(myBook.y !== undefined).toBe(true);  // !== can distinguish null and undefined
+        });
+
+        it('check if it is native property: hasOwnProperty', function(){
+            var myBook = inherit(book);
+            myBook.test = 'test';
+            expect(myBook.hasOwnProperty('title')).toBe(false);
+            expect(myBook.hasOwnProperty('test')).toBe(true);
+            expect(myBook.hasOwnProperty('notExists')).toBe(false);
+        });
     });
 
 });
