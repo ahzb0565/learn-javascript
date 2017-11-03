7ea799f09c58bb782bef43639a2e0ce115f42054
Bob <Bob@example.com>
2017-11-03 16:15:55 +0800

    finish chapter 8.

--- spec/chapter8.spec.js
+++ spec/chapter8.spec.js
@@ -146,7 +146,135 @@ describe('Chapter 8 Functions', function(){
             expect(funcs[5]()).toBe(10);
         });
     });
+    describe('8.7 methods, prototype and constructors', function(){
+        it('length', function(){
+            function test(){
+                return arguments.length;
+            }
+
+            expect(test()).toBe(0);
+            expect(test(1,2)).toBe(2);
+        });
+
+        it('prototype', function(){
+            function test(){};
+            expect(test.prototype).not.toBeUndefined();
+            expect(typeof test.prototype).toEqual('object');
+        });
+
+        it('call and apply', function(){
+            var o = {x: 1, y: 1, assign: function(x, y){this.x = x; this.y = y; return this;}};
+            var p = {x: 3, y: 4};
+            o.assign.call(p, 5,6);
+            expect(p.x).toBe(5);
+            expect(p.y).toBe(6);
+            o.assign.apply(p, [7, 8]);
+            expect(p.x).toBe(7);
+            expect(p.y).toBe(8);
+        });
+
+        it('bind', function(){
+            function test(y, z){return this.x + y + z;}
+            var newFunc = test.bind({x: 1});
+            expect(newFunc(2, 3)).toBe(6);
+            expect(newFunc(5, 6)).toBe(12);
+
+            var newFunc2 = test.bind({x: 3}, 2);  // bind '2' as parameter 'y'
+            expect(newFunc2(4)).toBe(9);  // only 'z' is needed. 3 + 2 + 4
+        });
+
+        it('toString', function(){});
+
+        it('Function() constructor', function(){
+            var f = new Function('x', 'y', 'return x + y');  // like eval in top function
+            expect(typeof f).toBe('function');
+
+            var scope = 'global';
+            var creator = function(){
+                var scope = 'local';
+                // return new Function('return scope;');  // new Function() will compile at the top, so it is 'global' or throw not found error
+            };
+        });
+
+        it('function like', function(){});
+    });
+
+    describe('8.8 functional programing', function(){
+        it('map reduce', function(){
+            // sum of square
+            var square = function(n){ return n*n;};
+            var sum = function(x, y){ return x+y;};
+
+            var data = [1,2,3,4,5];
+            expect(data.map(square).reduce(sum)).toBe(55);
+        });
+
+        it('higher-order function', function(){
+            function not(f){
+                return function(){
+                    var result = f.call(this, arguments);
+                    return !result;
+                };
+            }
+
+            function even(x){
+                return x % 2 === 0;
+            }
+
+            var odd = not(even);  // get a new function odd
+
+            expect([1,3,5,7,9].every(odd)).toBe(true);
+            expect([2,4,6,8,10].every(even)).toBe(true);
+        });
+
+        it('partial function', function(){
+            var f = function(x, y, z){return x * (y + z);};
+
+            var convertToArray = function(arrayLike, position){
+                /* convert array like object to real array */
+                return Array.prototype.slice.call(arrayLike, position || 0);
+            };
+
+            var partialLeft = function(f){
+                /* bind an argument to the left arguments of function f */
+                var args = arguments;
+                return function(){
+                    var a = convertToArray(args, 1);
+                    a = a.concat(convertToArray(arguments));
+                    return f.apply(this, a);
+                };
+            }
+
+            expect(f(2,3,4)).toBe(14);  // 2 * 3 + 4
+            expect(partialLeft(f, 2)(3,4)).toBe(14);  // 2 * 3 + 4
+            expect(partialLeft(f, 2, 3)(4)).toBe(14);  // 2 * 3 + 4
+        });
+
+        it('memorization', function(){
+            /* create a function that can cache results */
+            var memorize = function(f){
+                var cache = {};
+                return function(){
+                    var key = arguments.length + Array.prototype.join.call(arguments, ',')
+                    if (key in cache){
+                        console.log('found in cache');
+                        return cache[key];
+                    }
+                    result = f.apply(this, arguments);
+                    cache[key] = result;
+                    return result
+                };
+            }
+
+            var factorial = memorize(function(n){
+                return (n<=1)? 1: n*factorial(n-1);
+            });
+
+            expect(factorial(5)).toBe(120);
+            expect(factorial(3)).toBe(6);  // this will get result from cache.
+        });
 
 
+    })
 
 });
